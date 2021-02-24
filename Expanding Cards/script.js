b39e3dac33a646bee485a3a321fa2347d0cfcbc5
comboo <291376690@qq.com>
2021-02-25 00:24:40 +0800

    css 完成

--- Expanding Cards/script.js
+++ Expanding Cards/script.js
@@ -6,15 +6,19 @@ const panels = document.querySelectorAll(".panel")
 
 panels.forEach(function (panel) { 
     panel.addEventListener('click',function(event) {
-        debugger;
+   
         removeActiveClasses()
+       
+    
         event.target.classList.add("active")
+     
     }) 
 })
 
 
 function removeActiveClasses() {
     panels.forEach(panel => {
+     
         panel.classList.remove("active")
     })
 }
